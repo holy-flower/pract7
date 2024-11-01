ff594f59ee79cbc403c8b0cd2db90e74fed837aa
holy-flower <prodolnova04@mail.ru>
2024-11-01 10:53:16 +0300

    н

--- app/src/main/java/com/example/pract7/MainActivity.kt
+++ app/src/main/java/com/example/pract7/MainActivity.kt
@@ -9,10 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
